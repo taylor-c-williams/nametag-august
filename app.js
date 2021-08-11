ab82bb215709483c1c78dbf614ad4642c1d58d3f
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-08-11 16:15:53 -0700

    stretchy buttons

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const nameInput = document.getElementById("name-input");
+
 const nameButt = document.getElementById("name-butt");
+const greenButt = document.getElementById("green-butt");
+const purpleButt = document.getElementById("purple-butt");
+
 const middleSection = document.getElementById("middle-section");
 
 nameButt.addEventListener("click", () => {
 	middleSection.textContent = nameInput.value;
+});
+
+greenButt.addEventListener("click", () => {
+	middleSection.style.backgroundColor = "green";
+});
+
+purpleButt.addEventListener("click", () => {
+	middleSection.style.backgroundColor = "#3c096c";
 });
